7b93daaf4ed7f7b43e28bacf2613b542f1bbedf2
boosted-fire <33994778+boosted-fire@users.noreply.github.com>
2024-11-04 08:11:21 -0600

    Initial files added

---
+++ game.asm
@@ -0,0 +1,365 @@
+%include "/usr/local/share/csc314/asm_io.inc"
+
+; the file that stores the initial state
+%define BOARD_FILE 'board.txt'
+
+; how to represent everything
+%define WALL_CHAR '#'
+%define PLAYER_CHAR 'O'
+%define END_CHAR 'F'		; 11/1 nsp Added definition for maze finish
+
+; the size of the game screen in characters
+%define HEIGHT 20
+%define WIDTH 40
+
+; the player starting position.
+; top left is considered (0,0)
+%define STARTX 1
+%define STARTY 1
+
+; these keys do things
+%define EXITCHAR 'x'
+%define UPCHAR 'w'
+%define LEFTCHAR 'a'
+%define DOWNCHAR 's'
+%define RIGHTCHAR 'd'
+
+; the location of the trapdoors
+%define TDOOR_A 243		; location (3,6)	; 11/1 nsp added defined trapdoors - need to reconcile locations
+%define TDOOR_B 273		; location (33,6)
+%define TDOOR_C 385		; location (25,9)
+
+segment .data
+
+	; used to fopen() the board file defined above
+	board_file			db BOARD_FILE,0
+
+	; used to change the terminal mode
+	mode_r				db "r",0
+	raw_mode_on_cmd		db "stty raw -echo",0
+	raw_mode_off_cmd	db "stty -raw echo",0
+
+	; ANSI escape sequence to clear/refresh the screen
+	clear_screen_code	db	27,"[2J",27,"[H",0
+
+	; things the program will print
+	help_str			db 13,10,"Controls: ", \
+							UPCHAR,"=UP / ", \
+							LEFTCHAR,"=LEFT / ", \
+							DOWNCHAR,"=DOWN / ", \
+							RIGHTCHAR,"=RIGHT / ", \
+							EXITCHAR,"=EXIT", \
+							13,10,10,0
+
+	; messages to display at the end of the game
+	end_msg1 db "Congratulations - you finished the maze!  You fell through ",0		; 11/1 nsp addded message to display at the end of the game
+	end_msg2 db " trap doors.",0
+	trap_msg db "Oh no!  You fell through a trap door.  Returning to the start.",0
+
+segment .bss
+
+	; this array stores the current rendered gameboard (HxW)
+	board	resb	(HEIGHT * WIDTH)
+
+	; these variables store the current player position
+	xpos	resd	1
+	ypos	resd	1
+
+	tdoor_count resb 0		; 11/1 nsp counter variable added to track number of trap doors a user encounters
+
+segment .text
+
+	global	asm_main
+	global  raw_mode_on
+	global  raw_mode_off
+	global  init_board
+	global  render
+
+	extern	system
+	extern	putchar
+	extern	getchar
+	extern	printf
+	extern	fopen
+	extern	fread
+	extern	fgetc
+	extern	fclose
+
+asm_main:
+	push	ebp
+	mov		ebp, esp
+
+	; put the terminal in raw mode so the game works nicely
+	call	raw_mode_on
+
+	; read the game board file into the global variable
+	call	init_board
+
+	; set the player at the proper start position
+	mov		DWORD [xpos], STARTX
+	mov		DWORD [ypos], STARTY
+
+	; the game happens in this loop
+	; the steps are...
+	;   1. render (draw) the current board
+	;   2. get a character from the user
+	;	3. store current xpos,ypos in esi,edi
+	;	4. update xpos,ypos based on character from user
+	;	5. check what's in the buffer (board) at new xpos,ypos
+	;	6. if it's a wall, reset xpos,ypos to saved esi,edi
+	;	7. otherwise, just continue! (xpos,ypos are ok)
+	game_loop:
+
+		; draw the game board
+		call	render
+
+		; get an action from the user
+		call	getchar
+
+		; store the current position
+		; we will test if the new position is legal
+		; if not, we will restore these
+		mov		esi, DWORD [xpos]
+		mov		edi, DWORD [ypos]
+
+		; choose what to do
+		cmp		eax, EXITCHAR
+		je		game_loop_end
+		cmp		eax, UPCHAR
+		je 		move_up
+		cmp		eax, LEFTCHAR
+		je		move_left
+		cmp		eax, DOWNCHAR
+		je		move_down
+		cmp		eax, RIGHTCHAR
+		je		move_right
+		jmp		input_end			; or just do nothing
+
+		; move the player according to the input character
+		move_up:
+			dec		DWORD [ypos]
+			jmp		input_end
+		move_left:
+			dec		DWORD [xpos]
+			jmp		input_end
+		move_down:
+			inc		DWORD [ypos]
+			jmp		input_end
+		move_right:
+			inc		DWORD [xpos]
+		input_end:
+
+		; (W * y) + x = pos
+
+		; compare the current position to the wall character, end character  11/1 nsp update notes
+		mov		eax, WIDTH
+		mul		DWORD [ypos]
+		add		eax, DWORD [xpos]
+		lea		eax, [board + eax]
+		cmp		BYTE [eax], END_CHAR	; 11/1 nsp check to see if the user has arrived at the finish of the maze
+		je		game_complete			; 11/1 nsp jump to the end if the user has finished
+		cmp		BYTE [eax], WALL_CHAR
+		jne		valid_move
+			; opps, that was an invalid move, reset
+			mov		DWORD [xpos], esi
+			mov		DWORD [ypos], edi
+		valid_move:
+
+		; 11/1 nsp compare the new position to the trapdoors
+		cmp DWORD [eax], TDOOR_A
+		je trap_door
+		cmp DWORD [eax], TDOOR_B
+		je trap_door
+		cmp DWORD [eax], TDOOR_C
+		je trap_door
+
+		jmp		game_loop
+
+	game_complete:
+	mov eax, end_msg1		; 11/1 nsp load end message
+	call print_string		; 11/1 nsp print end message
+	mov eax, [tdoor_count]
+	call print_int
+	mov eax, end_msg2
+	call print_string
+	call print_nl			; 11/1 nsp pring new line
+
+	game_loop_end:
+
+	; restore old terminal functionality
+	call raw_mode_off
+
+	mov		eax, 0
+	mov		esp, ebp
+	pop		ebp
+	ret
+
+; if land on a trap door, notify the user, increment the trap door incident counter and return to start
+
+trap_door:
+	inc BYTE [tdoor_count]
+	mov eax, trap_msg
+	call print_string
+	call print_nl
+	mov DWORD [xpos], STARTX
+	mov DWORD [ypos], STARTY
+	jmp game_loop
+
+raw_mode_on:
+
+	push	ebp
+	mov		ebp, esp
+
+	push	raw_mode_on_cmd
+	call	system
+	add		esp, 4
+
+	mov		esp, ebp
+	pop		ebp
+	ret
+
+raw_mode_off:
+
+	push	ebp
+	mov		ebp, esp
+
+	push	raw_mode_off_cmd
+	call	system
+	add		esp, 4
+
+	mov		esp, ebp
+	pop		ebp
+	ret
+
+init_board:
+
+	push	ebp
+	mov		ebp, esp
+
+	; FILE* and loop counter
+	; ebp-4, ebp-8
+	sub		esp, 8
+
+	; open the file
+	push	mode_r
+	push	board_file
+	call	fopen
+	add		esp, 8
+	mov		DWORD [ebp - 4], eax
+
+	; read the file data into the global buffer
+	; line-by-line so we can ignore the newline characters
+	mov		DWORD [ebp - 8], 0
+	read_loop:
+	cmp		DWORD [ebp - 8], HEIGHT
+	je		read_loop_end
+
+		; find the offset (WIDTH * counter)
+		mov		eax, WIDTH
+		mul		DWORD [ebp - 8]
+		lea		ebx, [board + eax]
+
+		; read the bytes into the buffer
+		push	DWORD [ebp - 4]
+		push	WIDTH
+		push	1
+		push	ebx
+		call	fread
+		add		esp, 16
+
+		; slurp up the newline
+		push	DWORD [ebp - 4]
+		call	fgetc
+		add		esp, 4
+
+	inc		DWORD [ebp - 8]
+	jmp		read_loop
+	read_loop_end:
+
+	; close the open file handle
+	push	DWORD [ebp - 4]
+	call	fclose
+	add		esp, 4
+
+	mov		esp, ebp
+	pop		ebp
+	ret
+
+render:
+
+	push	ebp
+	mov		ebp, esp
+
+	; two ints, for two loop counters
+	; ebp-4, ebp-8
+	sub		esp, 8
+
+	; clear the screen
+	push	clear_screen_code
+	call	printf
+	add		esp, 4
+
+	; print the help information
+	push	help_str
+	call	printf
+	add		esp, 4
+
+	; outside loop by height
+	; i.e. for(c=0; c<height; c++)
+	mov		DWORD [ebp - 4], 0
+	y_loop_start:
+	cmp		DWORD [ebp - 4], HEIGHT
+	je		y_loop_end
+
+		; inside loop by width
+		; i.e. for(c=0; c<width; c++)
+		mov		DWORD [ebp - 8], 0
+		x_loop_start:
+		cmp		DWORD [ebp - 8], WIDTH
+		je 		x_loop_end
+
+			; check if (xpos,ypos)=(x,y)
+			mov		eax, DWORD [xpos]
+			cmp		eax, DWORD [ebp - 8]
+			jne		print_board
+			mov		eax, DWORD [ypos]
+			cmp		eax, DWORD [ebp - 4]
+			jne		print_board
+				; if both were equal, print the player
+				push	PLAYER_CHAR
+				call	putchar
+				add		esp, 4
+				jmp		print_end
+			print_board:
+				; otherwise print whatever's in the buffer
+				mov		eax, DWORD [ebp - 4]
+				mov		ebx, WIDTH
+				mul		ebx
+				add		eax, DWORD [ebp - 8]
+				mov		ebx, 0
+				mov		bl, BYTE [board + eax]
+				push	ebx
+				call	putchar
+				add		esp, 4
+			print_end:
+
+		inc		DWORD [ebp - 8]
+		jmp		x_loop_start
+		x_loop_end:
+
+		; write a carriage return (necessary when in raw mode)
+		push	0x0d
+		call 	putchar
+		add		esp, 4
+
+		; write a newline
+		push	0x0a
+		call	putchar
+		add		esp, 4
+
+	inc		DWORD [ebp - 4]
+	jmp		y_loop_start
+	y_loop_end:
+
+	mov		esp, ebp
+	pop		ebp
+	ret
